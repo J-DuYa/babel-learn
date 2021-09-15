4097ed071fe9978102239fb041051f5614f0e70b
duya <18795905365@163.com>
2021-09-15 13:54:07 +0800

    feature: 完善目录结构

--- example/config/webpack.config.js
+++ example/config/webpack.config.js
@@ -1,6 +1,34 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function resolve (p = '') {
+  return path.resolve(__dirname, p)
+}
+
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: resolve('../build'),
+    filename: 'babel-learn.min.js',
+  },
   devServer: {
     port: 7001
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.(css|less)$/i,
+      use: ['css-loader', 'style-loader', 'less-loader']
+    }, {
+      test: /\.(ts|tsx)$/i,
+      loader: 'ts-loader'
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html'
+    })
+  ]
 }
--- example/src/index.js
+++ example/src/index.js
@@ -1,3 +1,5 @@
+console.log('入口文件1');
+
 function a () {
   let b = 1;
   return b;
